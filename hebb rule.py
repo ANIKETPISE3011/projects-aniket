3eddb890330a398552cb5dab6016616aed6412e2
Aniket Pise <118692025+ANIKETPISE3011@users.noreply.github.com>
2022-11-29 16:44:34 +0530

    Add files via upload

---
+++ hebb rule.py
@@ -0,0 +1,12 @@
+X=[[1,1],[1,-1],[-1,1],[-1,-1]]
+print('inputs=')
+for x in X:
+    print(x)
+Y=[1,-1,-1,-1]
+print('target=',Y)
+w=[0,0]
+print('initial weight values=',w)
+for i in range(len(x)):
+    for j in range(len(w)):
+        w[j]=w[j]+X[i][j]*Y[i]
+    print(i,'Iteration , weight values=',w)
